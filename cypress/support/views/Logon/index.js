d95a13e453c41a53ecd588b5460beb3920ebbc3e
Jhonathan <jhonathanpribeiro@gmail.com>
2021-04-20 07:44:10 -0300

    update code

--- cypress/support/views/Logon/index.js
+++ cypress/support/views/Logon/index.js
@@ -12,6 +12,11 @@ class Logon {
         cy.get(el.usuario).type('idssaude').should('have.value', 'idssaude')
         // preenchendo o campo senha
         cy.get(el.senha).type('I@0309ds').should('have.value', 'I@0309ds')
+        ////btn acessar
+        cy.get('.btn-outline').click();
+         //unidade pré-selecionada, apenas clica novamente no botão de acessar
+         cy.wait(2000)
+         cy.get('.btn-outline').click();
     }
 }
 
