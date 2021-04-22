ccdc03cd2f23fbbfac45aba9247a59504b4519bd
Jhonathan <jhonathanpribeiro@gmail.com>
2021-04-22 03:16:45 -0300

    update files

--- cypress/support/views/Recepcao/elements.js
+++ cypress/support/views/Recepcao/elements.js
@@ -1,3 +1,19 @@
 export const ELEMENTS = {
-    
+    sidebar: ':nth-child(2) > a.ng-tns-c24-38',
+    viewRecepcao: '.active-menuitem > .ng-trigger > :nth-child(1) > .ng-star-inserted',
+    btnNew: '.pi-plus',
+    naoId: '.nao-identificado',
+    descricao: 'form.ng-untouched > :nth-child(1) > .p-grid > .p-col-12 > f17-input-container > div > .ui-inputtext',
+    sexo: ':nth-child(2) > .ng-untouched > .select-tab > .autocomplete-container > .auto-complete-select > .ui-autocomplete > .ui-autocomplete-dropdown > .ui-button-text',
+    opmasculino: '#p-highlighted-option',
+    idade: '.ng-invalid.ng-dirty > :nth-child(1) > .p-grid > :nth-child(3) > f17-input-container > div > .ui-inputtext',
+    racaoucor: '.ng-invalid > .select-tab > .autocomplete-container > .auto-complete-select > .ui-autocomplete > .ui-autocomplete-dropdown > .ui-button-text',
+    opracaoucor2: '.ui-autocomplete-items > :nth-child(2)',
+    endereco: ':nth-child(1) > :nth-child(3) > f17-input-container > div > .ui-inputtext',
+    observacoes: '.observacoes',
+    btnSave: '.btn-primary > .ui-button-text',
+    setorAtendimento: 'app-recepcao-setor > :nth-child(1) > .ng-untouched > .select-tab > .autocomplete-container > .auto-complete-select > .ui-autocomplete > .ui-autocomplete-dropdown > .ui-button-text',
+    primeiraOpcaoSetor: '.ui-autocomplete-items > :nth-child(1) > .ng-star-inserted > span',
+    btnSalvarRecepcao: 'f17-form-crud > .pi-check',
+    redirectViewListRecepcoes: '.ui-toolbar-group-left > a'
 }
